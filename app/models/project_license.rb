9bc6e7a66bd6b37f20811b6af5a6f5d875d0a362
Christian Wansart <christian@5dlab.com>
2013-08-20 23:11:52 +0200

    added validations and assiciation to project license

--- app/models/project_license.rb
+++ app/models/project_license.rb
@@ -1,2 +1,8 @@
 class ProjectLicense < ActiveRecord::Base
+  has_and_belongs_to_many :projects
+
+  attr_accessible :name, :license_url, :license_text
+  
+  validates :name, presence: true, uniqueness: true
+  validates :license_text, presence: { if: "license_url.blank?" }
 end
